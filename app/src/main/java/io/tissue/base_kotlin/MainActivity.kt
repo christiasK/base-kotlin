cc5a9ad5398fc4b10968e274b6eb5b01b2d35e5e
natoro <admin@natoro.com>
2020-08-23 11:11:41 +0900

    클릭 이벤트 발생 시 로그 기록

--- app/src/main/java/io/tissue/base_kotlin/MainActivity.kt
+++ app/src/main/java/io/tissue/base_kotlin/MainActivity.kt
@@ -1,11 +1,19 @@
 package io.tissue.base_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		clickMeBtn.setOnClickListener {
+			// Click Me 버튼이 클릭되면 실행될 코드 {}
+			// tag 어떤 종류의 로그인지?
+			// msg 로그 내용
+			Log.d("메인화면", "클릭미 버튼이 클릭됨")
+		}
 	}
 }
